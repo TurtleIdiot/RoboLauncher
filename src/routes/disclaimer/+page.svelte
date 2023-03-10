f585e5243a36d69cdfbf30ca3c22f487aab146ce
TurtleIdiot <18502738+TurtleIdiot@users.noreply.github.com>
2023-03-10 01:06:24 +0000

    Added FOSS info

--- src/routes/disclaimer/+page.svelte
+++ src/routes/disclaimer/+page.svelte
@@ -12,6 +12,7 @@
     <div class="card-body">
         <h1 class="card-title text-center"><strong><i>HEY, WAIT UP!</i></strong></h1>
         <p>This launcher is <strong><i>NOT</i></strong> official!<br/>This is a fan project and while it does aim to be feature-complete and maintained, it is not affiliated with FreeJam or Robocraft 2</p>
+        <p>This launcher is and always will be free and open source. If you paid for this, you got scammed</p>
         <p>To get the offical launcher or if youu're on windows, visit the official site <span><a href="https://www.robocraft2.com/" target="_blank" rel="noreferrer" class="link">here</a></span></p>
         <div class="card-actions justify-end">
             <button class="btn" on:click={() => {exit(0)}}>Quit</button>
