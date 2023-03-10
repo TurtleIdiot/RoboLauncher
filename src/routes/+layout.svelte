6d28e20a181af2fc79924e5ff5d039c10d6aefc8
TurtleIdiot <18502738+TurtleIdiot@users.noreply.github.com>
2023-03-10 22:07:17 +0000

    Fixed fontawesome js import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
 	import './styles.css';
+	import "$lib/fa/fontawesome.min.js"
+	import "$lib/fa/solid.min.js"
+	import "$lib/fa/brands.min.js"
 </script>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,7 +29,7 @@
         <div class="alert alert-warning shadow-lg fixed {errormsg !== null ? "bottom-20": "bottom-0"}">
             <div>
                 <!-- fontawesome solid triangle exclamation -->
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg>
+                <i class="fa-solid fa-triangle-exclamation"></i>
                 <span>{!dirSet ? "You do not have an install directory set" : infomsg}</span>
             </div>
             <div class="flex-none">
@@ -48,7 +48,7 @@
         <div class="alert alert-error shadow-lg fixed bottom-2">
             <div>
                 <!-- fontawesome solid triangle exclamation -->
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg>
+                <i class="fa-solid fa-triangle-exclamation"></i>
                 <span>{errormsg}</span>
             </div>
             <div class="flex-none">
